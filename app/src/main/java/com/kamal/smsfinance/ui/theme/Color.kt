e1b32ddce8971b317ca348541364fc8f98657223
kamal266-1 <kamal266@atomicmail.io>
2026-08-02 17:16:22 +0330

    add app/src/main/java/com/kamal/smsfinance/ui/theme/Color.kt

---
+++ app/src/main/java/com/kamal/smsfinance/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.kamal.smsfinance.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenIncome = Color(0xFF2E7D32)
+val GreenIncomeLight = Color(0xFF66BB6A)
+val RedExpense = Color(0xFFC62828)
+val RedExpenseLight = Color(0xFFEF5350)
+
+val PrimaryLight = Color(0xFF1565C0)
+val PrimaryDark = Color(0xFF90CAF9)
+
+val BackgroundLight = Color(0xFFF7F8FA)
+val BackgroundDark = Color(0xFF121212)
+
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
